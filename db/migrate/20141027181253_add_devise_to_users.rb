4f3ca45fe60152120aadbabaee46ea71f209f65c
Kgen Bao <kgen.bao@kingaxis.com>
2014-10-28 02:39:48 +0800

    Encrypt password with salt.

--- db/migrate/20141027181253_add_devise_to_users.rb
+++ db/migrate/20141027181253_add_devise_to_users.rb
@@ -4,6 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :username,           null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :password_salt
 
       ## Recoverable
       t.string   :reset_password_token
