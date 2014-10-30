cb80b6f35cb3e7df67f49a9adeb8daa6f73d720b
David Ruan <ruanwz@gmail.com>
2014-10-30 13:24:56 +0800

    Generate access_token

---
+++ db/migrate/20141030052402_create_access_tokens.rb
@@ -0,0 +1,13 @@
+class CreateAccessTokens < ActiveRecord::Migration
+  def change
+    create_table :access_tokens do |t|
+      t.references :user, index: true
+      t.string :token
+      t.datetime :expired_at
+      t.boolean :active
+      t.string :creator_ip
+
+      t.timestamps
+    end
+  end
+end
