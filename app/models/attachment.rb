6e117464a3cc21f81bcc8c5c37a617a82072c90b
David Ruan <ruanwz@gmail.com>
2014-10-30 15:03:41 +0800

    Link attachment and message

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,2 +1,4 @@
 class Attachment < ActiveRecord::Base
+  has_many :attachments_messages
+  has_many :messages, through: :attachments_messages
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :sender, class_name: User
   belongs_to :recipient, polymorphic: true
+  has_many :attachments_messages
+  has_many :attachments, through: :attachments_messages
+
 end
