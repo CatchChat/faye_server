1692956b7260aaf9e2b2ddf60856e6d9f3eb2187
David Ruan <ruanwz@gmail.com>
2014-10-30 13:16:21 +0800

    Generate Message

---
+++ db/migrate/20141030051159_create_messages.rb
@@ -0,0 +1,18 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.references :user, index: true
+      t.integer :recipient
+      t.string :recipient_type
+      t.string :media_type
+      t.text :text_content
+      t.integer :parent_id
+      t.string :state
+
+      t.timestamps
+    end
+    add_index :messages, :user_id
+    add_index :messages, :recipient
+    add_index :messages, :parent_id
+  end
+end
