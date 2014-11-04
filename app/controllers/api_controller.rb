d4434c95d3748e1f3051b08463a26af7a26726ee
tumayun <tumayun.2010@gmail.com>
2014-11-04 15:47:15 +0800

    Rename home_controller to api_controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,18 @@
+class ApiController < ApplicationController
+  include AuthToken
+
+  before_action :set_locale
+  before_action :authenticate_user
+
+  def authenticate_user
+    render json: { error: "Unauthorized!" }, status: 401 unless authenticated?
+  end
+
+  def set_locale
+    if request.headers['Accept-Language'].present? &&
+      I18n.available_locales.include?(request.headers['Accept-Language'].to_sym)
+      I18n.locale = locale
+    end
+    logger.debug "===> Set locale to #{I18n.locale}."
+  end
+end
