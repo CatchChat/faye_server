bb9aec26ba6d172e865161b16c4c0da4fa3cce70
David Ruan <ruanwz@gmail.com>
2014-11-05 15:20:07 +0800

    Fix unique mobile issue in test

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -4,6 +4,7 @@ describe Users::SessionsController do
   render_views
   before do
     @user   = FactoryGirl.create(:user)
+    @mobile_user   = FactoryGirl.create(:mobile_user)
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
@@ -21,7 +22,7 @@ describe Users::SessionsController do
 
   it 'logins with mobile and sms verification code then returns token json' do
     post :create, login: '123456789', password: 'test-token', :format => 'json'
-    expect(response.body).to include 'ruanwztest'
+    expect(response.body).to include 'mobile'
     expect(response.body).to include 'test-token'
 
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,18 +3,24 @@ FactoryGirl.define do
   factory :user do
     username 'ruanwztest'
     password 'ruanwztest'
-    mobile '123456789'
     node_id '542a22ae4b44684f2ecb2398'
     node_password '62d30f88375b7f4f1461aa0e19b47e6e52c6141409a8c5e6bcb2c45e8186a4a1'
     node_token 'o1U8LNPvh1VX0R0M'
     access_token
-    sms_verification_code
 
   end
   factory :access_token do
     token 'test-token'
   end
 
+  factory :mobile_user, class: User do
+    username 'ruanwzmobile'
+    password 'ruanwzmobile'
+    node_id '542a22ae4b44684f2ecb2398'
+    node_password '62d30f88375b7f4f1461aa0e19b47e6e52c6141409a8c5e6bcb2c45e8186a4a1'
+    sms_verification_code
+    access_token
+  end
   factory :sms_verification_code do
     mobile '123456789'
     token 'test-token'
