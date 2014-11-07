92627f237ab59c7ae0639eb26047f5af8c5460b3
David Ruan <ruanwz@gmail.com>
2014-11-07 13:12:30 +0800

    Generate token with expired_at according to login params

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,11 +16,14 @@ class Users::SessionsController < Devise::SessionsController
 
   def get_access_token
     @user = current_user
+
     token = AccessToken.create user_id: @user.id,
       active: true,
-      token: @user.generate_token
+      token: @user.generate_token,
+      expired_at: get_expired_at
     @access_token = token
   end
+
 
   # Post auth/send_verify_code
   def send_verify_code
@@ -36,6 +39,11 @@ class Users::SessionsController < Devise::SessionsController
   end
 
 
+  private
+  def get_expired_at
+    valid_period = params.fetch(:expiring, 7*24*3600).to_i
+    valid_period == 0 ? nil : Time.now + valid_period
+  end
 
   # protected
 
--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -7,13 +7,32 @@ describe Users::SessionsController do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
-  it 'logins with username and password then returns token json' do
-    post :create,login: 'ruanwztest', password: 'ruanwztest', :format => 'json'
+  it 'logins with username/password and expiring 0 then returns always active token json' do
+    post :create,login: 'ruanwztest', password: 'ruanwztest', expiring: 0, :format => 'json'
+    expect(@user.access_tokens.last.active).to be true
+    expect(@user.access_tokens.last.expired_at).to be nil
     expect(response.body).to include 'ruanwztest'
     expect(response.body).to include 'access_token'
     expect(response.body).not_to include 'null'
   end
 
+  it 'logins with username/password and no expiring then returns 7 days active token json' do
+    post :create,login: 'ruanwztest', password: 'ruanwztest', :format => 'json'
+    expect(@user.access_tokens.last.active).to be true
+    expect(@user.access_tokens.last.expired_at.to_i/100).to eq (Time.now.to_i + 3600*24*7)/100
+    expect(response.body).to include 'ruanwztest'
+    expect(response.body).to include 'access_token'
+    expect(response.body).not_to include 'null'
+  end
+
+  it 'logins with username/password and expiring then returns exact expired_at active token json' do
+    post :create,login: 'ruanwztest', password: 'ruanwztest', expiring: 1000, :format => 'json'
+    expect(@user.access_tokens.last.active).to be true
+    expect(@user.access_tokens.last.expired_at.to_i/10).to eq (Time.now.to_i + 1000)/10
+    expect(response.body).to include 'ruanwztest'
+    expect(response.body).to include 'access_token'
+    expect(response.body).not_to include 'null'
+  end
   it 'logins with node token then returns token json' do
     post :create, login: 'ruanwztest', password: 'node', :format => 'json'
     expect(response.body).to include 'ruanwztest'
