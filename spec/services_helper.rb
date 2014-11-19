cbcd2a8648da3026dd590da902a1888b401bb619
David Ruan <ruanwz@gmail.com>
2014-11-19 11:27:08 +0800

    Add callback config to env

--- spec/services_helper.rb
+++ spec/services_helper.rb
@@ -1,5 +1,9 @@
 ENV["qiniu_access_key"]="BBHE3ccYQ8VQhEIvZbJARrte1U3ic2Om6CW7mxvN"
 ENV["qiniu_secret_key"]="pQ5v3Bpg6AIx5hIXo9fXJgCLjgObH-DjKWOWU8dy"
+
+ENV["qiniu_callback_url"]="http://ruanwz.ngrok.com/hi"
+ENV["qiniu_callback_body"]="key=$(key)&bucket=$(bucket)"
+
 ENV["AWS_ACCESS_KEY_ID"]="AKIAOGBVMZAU5EZPGPIQ"
 ENV["AWS_SECRET_ACCESS_KEY"]="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"
 ENV["AWS_REGION"]='cn-north-1'
