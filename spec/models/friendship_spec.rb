83e0c802a957028f6727cbacb937c57f7eef5bf7
tumayun <tumayun.2010@gmail.com>
2014-11-20 18:50:20 +0800

    Add test code for friendship model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -4,4 +4,31 @@ require 'timecop'
 RSpec.describe Friendship, :type => :model do
   let(:user) { FactoryGirl.create(:user, username: 'user') }
   let(:friend) { FactoryGirl.create(:user, username: 'friend') }
+
+  it '.create_friendships' do
+    expect(Friendship.create_friendships(user, friend)).to eq true
+    expect(user.friends).to include friend
+    expect(friend.friends).to include user
+  end
+
+  it '.unfriend' do
+    expect(Friendship.create_friendships(user, friend)).to eq true
+    expect(user.friends).to include friend
+    expect(friend.friends).to include user
+    Friendship.unfriend(user, friend)
+    expect(user.friends).to_not include friend
+    expect(friend.friends).to_not include user
+  end
+
+  it '#name' do
+    expect(Friendship.create_friendships(user, friend)).to eq true
+    friendship = Friendship.find_by(user: user, friend: friend)
+    expect(friendship.name).to eq 'friend'
+    friend.update!(nickname: 'nickname')
+    expect(friendship.reload.name).to eq 'nickname'
+    friendship.update!(contact_name: 'contact_name')
+    expect(friendship.reload.name).to eq 'contact_name'
+    friendship.update!(remarked_name: 'remarked_name')
+    expect(friendship.reload.name).to eq 'remarked_name'
+  end
 end
