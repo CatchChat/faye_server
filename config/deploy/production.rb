790293b93f87e634f22e06a6803afdfa392c71cd
David Ruan <ruanwz@gmail.com>
2014-11-25 10:19:29 +0800

    Rename server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'production.catchchat_server.me', user: 'deploy', roles: %w{web app db}
+server 'production.catchchat-server.me', user: 'deploy', roles: %w{web app db}
 
 
 # Custom SSH Options
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -11,7 +11,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'staging.catchchat_server.me', user: 'deploy', roles: %w{web app db}
+server 'staging.catchchat-server.me', user: 'deploy', roles: %w{web app db}
 
 
 # Custom SSH Options
