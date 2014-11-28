ef4d44e1053d36b5b40ca9534cdf05c8d1e31632
David Ruan <ruanwz@gmail.com>
2014-11-28 15:34:11 +0800

    Need to know if attachment is private or public like avatar

--- db/migrate/20141030051934_create_attachments.rb
+++ db/migrate/20141030051934_create_attachments.rb
@@ -1,10 +1,11 @@
 class CreateAttachments < ActiveRecord::Migration
   def change
     create_table :attachments do |t|
-      t.string :storage
-      t.string :file
-      t.string :fallback_storage
-      t.string :fallback_file
+      t.string  :storage
+      t.string  :file
+      t.string  :fallback_storage
+      t.string  :fallback_file
+      t.boolean :public, default: false, null: false
 
       t.timestamps
     end
