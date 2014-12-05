88450187d9f081a1cae73673f37fc05ad5f54597
David Ruan <ruanwz@gmail.com>
2014-12-05 16:07:32 +0800

    Add slack notify

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,22 @@ set :sidekiq_monit_default_hooks, -> { true }
 
 ### slack
 set :slack_url, 'https://hooks.slack.com/services/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'
-
+set :slack_channel, '#server-side'
+set :slack_username, 'Deploybot'
+set :slack_emoji, ':trollface:'
+set :slack_user, ENV['GIT_AUTHOR_NAME']
+set :slack_text, -> {
+  elapsed = Integer(fetch(:time_finished) - fetch(:time_started))
+  "Revision #{fetch(:current_revision, fetch(:branch))} of " \
+  "#{fetch(:application)} deployed to #{fetch(:stage)} by #{fetch(:slack_user)} " \
+  "in #{elapsed} seconds."
+}
+set :slack_deploy_starting_text, -> {
+  "#{fetch(:stage)} deploy starting with revision/branch #{fetch(:current_revision, fetch(:branch))} for #{fetch(:application)}"
+}
+set :slack_deploy_failed_text, -> {
+  "#{fetch(:stage)} deploy of #{fetch(:application)} with revision/branch #{fetch(:current_revision, fetch(:branch))} failed"
+}
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
