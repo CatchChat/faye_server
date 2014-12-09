3121fed6829c0be15868f5e8d61d5ecb068aecb3
David Ruan <ruanwz@gmail.com>
2014-12-09 14:05:33 +0800

    only pass the attachment attrs hash

--- app/jobs/transfer_attachments_job.rb
+++ app/jobs/transfer_attachments_job.rb
@@ -2,7 +2,7 @@ class TransferAttachmentsJob
   include Sidekiq::Worker
   sidekiq_options queue: :attachments, :retry => 3
 
-  def perform(*args)
+  def perform(args)
     id = args["id"].to_i
     attachment = Attachment.find(id)
     AttachmentTransfer.transfer_s3 attachment
