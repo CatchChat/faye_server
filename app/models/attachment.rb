fd37d6dc251deec6dafcb462f3cfa81e2460f150
David Ruan <ruanwz@gmail.com>
2014-12-11 11:02:16 +0800

    parsing existing private qiniu attachment url (must in the same bucket)

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,6 @@
 class Attachment < ActiveRecord::Base
+
+  class AttachmentParsingError < RuntimeError; end
   has_and_belongs_to_many :messages
 
   validates :file, presence: true
@@ -16,5 +18,17 @@ class Attachment < ActiveRecord::Base
     return [expires_in, nil] unless fallback_storage == 's3'
     cdn = S3Helper.client
     [expires_in, cdn.get_download_url(key: fallback_file, download_expires_in: expires_in)]
+  end
+
+  def self.create_by_parsing_qiniu_private_url(url)
+    uri = URI.parse url
+    host = uri.host
+    raise AttachmentParsingError, 'only work for qiniu url' unless host =~ /qiniudn.com/
+    bucket = host.split('.').first
+    path = uri.path
+    key = path[1..-1]
+    raise AttachmentParsingError, "must set to the same bucket before parsing" unless bucket == ENV["qiniu_attachment_bucket"]
+
+    self.create! storage: 'qiniu', file: key
   end
 end
