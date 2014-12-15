87490deadddedc34b18103c47c0208b8f094b632
David Ruan <ruanwz@gmail.com>
2014-12-15 16:12:21 +0800

    add more friends for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,13 +24,20 @@ friend = User.create username:       'tumayun',
                      phone_code:     '86',
                      node_password:  '62d30f88375b7f4f1461aa0e19b47e6e52c6141409a8c5e6bcb2c45e8186a4a1'
 
+friend2 = User.create username:      'nick',
+                     password:       '123456',
+                     mobile:         '876543210',
+                     phone_code:     '86',
+                     node_password:  '62d30f88375b7f4f1461aa0e19b47e6e52c6141409a8c5e6bcb2c45e8186a4a1'
 friend = User.create username:       'catchchat',
                      password:       '123456',
                      mobile:         '876543210',
                      phone_code:     '86',
                      node_password:  '62d30f88375b7f4f1461aa0e19b47e6e52c6141409a8c5e6bcb2c45e8186a4a1'
 friend_request = user.friend_requests.create!(friend_id: friend.id)
+friend_request2 = user.friend_requests.create!(friend_id: friend2.id)
 friend_request.accept!
+friend_request2.accept!
 
 group = user.groups.create!(name: 'group')
 group.friendships << user.friendships.last
