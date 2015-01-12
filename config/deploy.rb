10342aca106d99d82bf017dc833769833e6486b7
David Ruan <ruanwz@gmail.com>
2015-01-12 14:09:03 +0800

    fayer config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,20 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'faye_server'
+set :repo_url, 'git@github.com:CatchChat/catchchat_server.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/u/apps/faye_server'
 
 # Default value for :scm is :git
 # set :scm, :git
-
+set :ssh_options, {
+  forward_agent: true
+}
 # Default value for :format is :pretty
 # set :format, :pretty
 
@@ -32,7 +34,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
