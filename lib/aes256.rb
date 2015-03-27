30e4e1af8577b7dcbfd3c4bf5b938e183bf078b6
tumayun <tumayun.2010@gmail.com>
2015-03-27 20:50:51 +0800

    Replace hard-coded to use environment variables

--- lib/aes256.rb
+++ lib/aes256.rb
@@ -3,18 +3,16 @@ class AES256
     def encrypt(data)
       cipher = OpenSSL::Cipher::AES.new(256, :CBC)
       cipher.encrypt
-      # FIXME
-      cipher.key = "w\u009E\u0083\u0000éfÆWÉ\u0006\t®ªæm\u0098\u00118l\u001F¤êþC\u0006°Uw±Wci"
-      cipher.iv  = "EKø«CtálÔ\u0097\u000FoÆ\v%n"
+      cipher.key = ENV['AES256_KEY']
+      cipher.iv  = ENV['AES256_IV']
       cipher.update(data) << cipher.final
     end
 
     def decrypt(data)
       cipher = OpenSSL::Cipher::AES.new(256, :CBC)
       cipher.decrypt
-      # FIXME
-      cipher.key = "w\u009E\u0083\u0000éfÆWÉ\u0006\t®ªæm\u0098\u00118l\u001F¤êþC\u0006°Uw±Wci"
-      cipher.iv  = "EKø«CtálÔ\u0097\u000FoÆ\v%n"
+      cipher.key = ENV['AES256_KEY']
+      cipher.iv  = ENV['AES256_IV']
       cipher.update(data) << cipher.final
     end
   end
